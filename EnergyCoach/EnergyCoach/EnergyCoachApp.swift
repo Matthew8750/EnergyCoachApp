0e642557dfd5441eb2fbfd8774b71e5fa3b0c3e8
helen robinson <helenrobinson@helens-MacBook-Pro.local>
2026-07-11 13:35:52 +0100

    Add EnergyCoach iOS app shell

---
+++ EnergyCoach/EnergyCoach/EnergyCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyCoachApp.swift
+//  EnergyCoach
+//
+//  Created by helen robinson on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct EnergyCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
